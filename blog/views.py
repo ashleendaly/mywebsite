af8ec5c206c27fda04696c2b9155037543b51075
Ashleen Daly <2552805d@student.gla.ac.uk>
2022-03-23 15:56:19 +0000

    added categories to context_dict in index view

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post, Category
 
 
 def index(request):
-    context_dict = {'boldmessage': ' How I Made A Blog For Free Using Django'}
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = ' How I Made A Blog For Free Using Django'
+    context_dict['categories'] = category_list
+
     return render(request, 'blog/index.html', context=context_dict)
 
 
